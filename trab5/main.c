38163c173d458e1d400b9ebf3f19e85a3378a8a0
Gustavo Rodrigues <gustavo._.r@hotmail.com>
2016-06-03 01:10:31 -0300

    modificado, necessita apenas da montagem das chaves

--- trab5/main.c
+++ trab5/main.c
@@ -4,38 +4,57 @@
 
 #include "squareMultiply.c"
 
-int main(){
+// Decriptografar a mensagem C utilizando N e D (onde D é a chave secreta):
+// M = C^D (mod N)
+// Criptografar a mensagem M utilizando N e E (onde E é a chave pública*):
+// C = M^E (mod N)
+// * É a chave pública mas também pode ser a secreta, porém nesse caso utilizaremos apenas como pública
+
+// Usage: <programName.c> <1:create keys - 0:inform keys> <c:crypt - d:decrypt> <plain/cypherTXT> <optional:base (default: 10)>
+
+int main(int argc, char *argv[]){
   int base;
-  mpz_t modulus, expoent, plainText, cipherText, result;
-  mpz_inits(modulus, expoent, plainText, cipherText,result, NULL);
+  long long int N, publicK, secretK;
+  mpz_t modulus, expoent, textIn, result;
+  mpz_inits(modulus, expoent, textIn, result, NULL);
 
-  scanf("%d", &base);
+  if(argc == 5)
+    base = argv[4];
+  else
+    base = 10;
 
-  if(base == 16){
-    gmp_scanf("%Zx", modulus);
-    gmp_scanf("%Zx", expoent);
-    gmp_scanf("%Zx", plainText);
-    gmp_scanf("%Zx", cipherText);
+  mpz_set_str(textIn, argv[3], base);
+
+  // Seleciona se a escolha foi criar as chaves ou informar
+  if(argv[1] == 1){
+    generateK(&N, &publicK, &secretK);
+    printf("Public Key generated: %llu\n", publicK);
+    printf("Secret Key generated: %llu\n", secretK);
+    mpz_set_ui(modulus, N);
+    if(argv[2] == 'c')
+      mpz_set_ui(expoent, publicK);
+    else
+      mpz_set_ui(expoent, secretK);
   }else{
-    gmp_scanf("%Zd", modulus);
-    gmp_scanf("%Zd", expoent);
-    gmp_scanf("%Zd", plainText);
-    gmp_scanf("%Zd", cipherText);
+    if(base == 16){
+      gmp_scanf("%Zx", modulus);
+      gmp_scanf("%Zx", expoent);
+    }else{
+      gmp_scanf("%Zd", modulus);
+      gmp_scanf("%Zd", expoent);
+    }
   }
 
-  sam(expoent, plainText, modulus, result);
+  sam(expoent, textIn, modulus, result);
 
   if(base == 16)
     gmp_printf("%Zx\n", result);
   else
     gmp_printf("%Zd\n", result);
 
-  if(mpz_cmp(cipherText, result) == 0){
-    printf("It is equal\n");
-  }else{
-    printf("something went wrong\n");
-  }
+  mpz_clears(modulus, expoent, textIn, result, NULL);
+}
 
-  mpz_clears(modulus, expoent, plainText, cipherText, result, NULL);
-
+void generateK(long long int *N, long long int *publicK, long long int *secretK){
+  
 }
